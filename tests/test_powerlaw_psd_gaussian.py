5dfa24d82f4956a3702d9af20e41f468cba5a75e
Felix Patzelt <felix@neuro.uni-bremen.de>
2017-09-23 16:58:55 +0200

    Small improvements to tests

--- tests/test_powerlaw_psd_gaussian.py
+++ tests/test_powerlaw_psd_gaussian.py
@@ -1,7 +1,13 @@
 import unittest
+import numpy as np
 import colorednoise as cn
 
 class TestPowerlawPsdGaussian(unittest.TestCase):
+    # these only test very basic functioning as of yet
     def test_powerlaw_psd_gaussian_output_shape(self):
-        n = cn.powerlaw_psd_gaussian(1, 100)
-        self.assertEqual(n.shape, (100,))
+        n = cn.powerlaw_psd_gaussian(1, 16)
+        self.assertEqual(n.shape, (16,))
+
+    def test_powerlaw_psd_gaussian_output_finite(self):
+        n = cn.powerlaw_psd_gaussian(1, 16)
+        self.assertTrue(np.isfinite(n).all())
