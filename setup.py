8d0365c687d9da09f70b915856bec53b813a94d0
Felix Patzelt <felix@neuro.uni-bremen.de>
2017-09-29 16:21:58 +0200

    formatting fix

--- setup.py
+++ setup.py
@@ -13,17 +13,18 @@ setup(
     """,
     classifiers=[
       'Development Status :: 5 - Production/Stable',
-'Intended Audience :: Developers',
-'Intended Audience :: Education',
-'Intended Audience :: Science/Research',
+      'Intended Audience :: Developers',
+      'Intended Audience :: Education',
+      'Intended Audience :: Science/Research',
       'License :: OSI Approved :: MIT License',
       'Programming Language :: Python :: 2',
       'Programming Language :: Python :: 2.7',
       'Programming Language :: Python :: 3',
       'Programming Language :: Python :: 3.6',
-      'Topic :: Scientific/Engineering'
-'Topic :: Software Development :: Libraries'
-'Topic :: Software Development :: Libraries :: Python Modules'    ],
+      'Topic :: Scientific/Engineering',
+      'Topic :: Software Development :: Libraries',
+      'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
     keywords='1/f flicker power-law correlated colored noise generator',
     url='http://github.com/felixpatzelt/colorednoise',
     download_url=(
